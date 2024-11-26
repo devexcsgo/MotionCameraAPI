246601bc576494ff7c13c8538e555aec5ad87b6f
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-11-26 11:21:05 +0100

    added noter til repository

--- MotionCameraAPI/LicensePlateRepository.cs
+++ MotionCameraAPI/LicensePlateRepository.cs
@@ -4,5 +4,21 @@
     {
         private int _nextId = 1;
         private readonly List<LicensePlate> _licensePlates = new List<LicensePlate>();
+
+        public LicensePlateRepository()
+        {
+            // Mock data
+            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "ABC123", Time = DateTime.Now });
+            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "DEF456", Time = DateTime.Now });
+        }
+
+        // Add
+
+        // Get
+
+        // Get all
+
+        // Delete
+
     }
 }
