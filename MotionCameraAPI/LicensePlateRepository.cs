92bc05b41a124c20482ca0b8002a47af6b2c69d2
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-11-26 14:42:13 +0100

    Merging conflicts. Kommenteret mockdata og tilføjet GetByID i stedet for Get

--- MotionCameraAPI/LicensePlateRepository.cs
+++ MotionCameraAPI/LicensePlateRepository.cs
@@ -7,18 +7,32 @@
 
         public LicensePlateRepository()
         {
-            // Mock data
-            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "ABC123", Time = DateTime.Now });
-            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "DEF456", Time = DateTime.Now });
+            //// Mock data
+            //_licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "DW74810", Time = DateTime.Now });
+            //_licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "BT24209", Time = DateTime.Now });
         }
 
         // Add
 
         // Get
+        public LicensePlate GetById(int id)
+        {
+            return _licensePlates.FirstOrDefault(lp => lp.Id == id);
+        }
 
         // Get all
 
         // Delete
+        public LicensePlate? Remove(int id)
+        {
+            LicensePlate? licensePlate = GetById(id);
+            if (licensePlate == null)
+            {
+                return null;
+            }
+            _licensePlates.Remove(licensePlate);
+            return licensePlate;
+        }
 
     }
 }
