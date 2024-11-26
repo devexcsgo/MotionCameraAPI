dfb216b965c6902853bc307c75dcc8e449f43ed3
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-11-26 12:13:00 +0100

    Tilføjet Add, Get, Getall, Delete

--- MotionCameraAPI/LicensePlateRepository.cs
+++ MotionCameraAPI/LicensePlateRepository.cs
@@ -8,17 +8,43 @@
         public LicensePlateRepository()
         {
             // Mock data
-            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "ABC123", Time = DateTime.Now });
-            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "DEF456", Time = DateTime.Now });
+            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "DW74810", Time = DateTime.Now });
+            _licensePlates.Add(new LicensePlate { Id = _nextId++, Plate = "BT24209", Time = DateTime.Now });
         }
 
         // Add
+        public LicensePlate Add(LicensePlate licensePlate)
+        {
+            // Mangler validering
+            licensePlate.Id = _nextId++;
+            _licensePlates.Add(licensePlate);
+            return licensePlate;
+        }
 
         // Get
+        public LicensePlate Get(int id)
+        {
+            return _licensePlates.FirstOrDefault(lp => lp.Id == id);
+        }
 
         // Get all
+        public IEnumerable<LicensePlate> GetAll()
+        {
+            IEnumerable<LicensePlate> allPlatesList = new List<LicensePlate>(_licensePlates);
+            return allPlatesList;
+        }
 
         // Delete
+        public LicensePlate? Remove(int id)
+        {
+            LicensePlate? licensePlate = Get(id);
+            if (licensePlate == null)
+            {
+                return null;
+            }
+            _licensePlates.Remove(licensePlate);
+            return licensePlate;
+        }
 
     }
 }
