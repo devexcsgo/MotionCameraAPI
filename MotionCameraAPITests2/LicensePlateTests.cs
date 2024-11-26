b9429990b7800e97609210e8caa69c561eb5df88
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-11-26 11:23:27 +0100

    Lavet tomt unit testing i branch

---
+++ MotionCameraAPITests2/LicensePlateTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MotionCameraAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotionCameraAPI.Tests
+{
+    [TestClass()]
+    public class LicensePlateTests
+    {
+        [TestMethod()]
+        public void ValidatePlateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
