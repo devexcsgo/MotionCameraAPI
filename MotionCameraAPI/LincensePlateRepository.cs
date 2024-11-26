26ed0502796e4972d3bd0deed8301d964314db8c
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-11-26 10:49:25 +0100

    Tilføjet 2 klasser

---
+++ MotionCameraAPI/LincensePlateRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace MotionCameraAPI
+{
+    public class LincensePlateRepository
+    {
+        public int Id { get; set; }
+        public string? Plate { get; set; }
+        public DateTime? Time { get; set; }
+    }
+}
