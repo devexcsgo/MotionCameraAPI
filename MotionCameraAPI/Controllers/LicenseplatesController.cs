efc3e773ad1e2471f5f54c8030dffb2c07bc53f0
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-11-26 10:27:15 +0100

    Add project files.

---
+++ MotionCameraAPI/Controllers/LicenseplatesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MotionCameraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LicenseplatesController : ControllerBase
+    {
+        // GET: api/<LicenseplatesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LicenseplatesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LicenseplatesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LicenseplatesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LicenseplatesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
