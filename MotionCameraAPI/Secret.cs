64663b5ceff58c38c87f547b7f079c962cf00940
Silas Stilling-Jørgensen <sis002@edu.zealand.dk>
2024-12-06 11:29:02 +0100

    secret included

---
+++ MotionCameraAPI/Secret.cs
@@ -0,0 +1,12 @@
+﻿namespace MotionCameraAPI
+{
+    public class Secret
+    {
+        private static string connectionString = @"Data Source=mssql4.unoeuro.com;Initial Catalog=silasstilling_dk_db_gproser;Persist Security Info=True;User ID=silasstilling_dk;Password=cxenHybp2DFGf9Rtz6aw;TrustServerCertificate=True";
+
+        public static string ConnectionString
+        {
+            get { return connectionString; }
+        }
+    }
+}
